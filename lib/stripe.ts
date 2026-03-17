83ea52039280499379c5355b97957e36c9beb9ad
Quin Elson <qelson@protonmail.com>
2026-03-17 07:01:10 -0400

    Fix Stripe initialization for build time

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,18 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-02-25.clover',
+let _stripe: Stripe | null = null
+
+export function getStripe(): Stripe {
+  if (!_stripe) {
+    _stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+      apiVersion: '2026-02-25.clover' as any,
+    })
+  }
+  return _stripe
+}
+
+export const stripe = new Proxy({} as Stripe, {
+  get(_, prop) {
+    return getStripe()[prop as keyof Stripe]
+  }
 })
